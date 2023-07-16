11a74864a4347b9e5c3b9416e10815e10fb9a79d
Olesya Grishina <olesyagrishina@mail.ru>
2023-07-16 17:42:33 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int water = 3_000;
+        int eaters = 5;
+        int potatoes = 5;
+        int chicken = 6;
+        int spices = 10;
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((potatoes / eaters) + " картофелин(а/ы)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
+            }
+
+}
